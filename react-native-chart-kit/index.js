3c1e2e0386c192f8f8f405370ec21d1a009a01fa
Pod Foods <podfoods@Pods-MacBook-Pro.local>
2021-03-25 17:22:16 +0700

    add chart kit

---
+++ react-native-chart-kit/index.js
@@ -0,0 +1,17 @@
+import AbstractChart from "./src/AbstractChart";
+import LineChart from "./src/line-chart";
+import BarChart from "./src/BarChart";
+import PieChart from "./src/PieChart";
+import ProgressChart from "./src/ProgressChart";
+import ContributionGraph from "./src/contribution-graph";
+import StackedBarChart from "./src/StackedBarChart";
+
+export {
+  AbstractChart,
+  BarChart,
+  LineChart,
+  PieChart,
+  ProgressChart,
+  ContributionGraph,
+  StackedBarChart
+};
